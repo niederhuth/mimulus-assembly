1855e407c64b62e7a730bdfea87d929db8fe7211
Niederhuth <niederhu@msu.edu>
2021-06-26 19:46:27 -0400

    stuff

--- scripts/assembly/download_microbe_database.sh
+++ scripts/assembly/download_microbe_database.sh
@@ -5,7 +5,11 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10GB
 #SBATCH --job-name download_microbe_database
-#SBATCH --output=job_reports/%x-%j.SLURMout
+#SBATCH --output=%x-%j.SLURMout
+
+
+mkdir microbe-database
+cd microbe-database
 
 #download files
 echo "Downloading sbt.gtdb-rs202.genomic.k31"
@@ -14,6 +18,8 @@ wget -O sbt.gtdb-rs202.genomic.k31.zip https://osf.io/dmsz8/download
 #unzip files
 echo "Unzipping sbt.gtdb-rs202.genomic.k31.zip"
 unzip sbt.gtdb-rs202.genomic.k31.zip
+
+rm unzip sbt.gtdb-rs202.genomic.k31.zip
 
 echo "Down"
 
