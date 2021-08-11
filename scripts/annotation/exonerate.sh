94e7b54e6a2030100897b352225c3812eb7eddec
Niederhuth <niederhu@msu.edu>
2021-08-11 10:03:41 -0400

    stuff

--- scripts/annotation/exonerate.sh
+++ scripts/annotation/exonerate.sh
@@ -10,9 +10,14 @@
 #Set this variable to the path to wherever you have conda installed
 conda="${HOME}/miniconda3"
 
+#Protein source
+protein="Mguttatus/IM62/ref/annotations/IM62-v2-protein.fa"
+
 #Set variables
 threads=40
-protein="Mguttatus/IM62/ref/annotations/IM62-v2-protein.fa"
+minintron=10
+maxintron=3000
+bestn=5
 ryo="no" #">%qi length=%ql alnlen=%qal\n>%ti length=%tl alnlen=%tal\n"
 
 #Change to current directory
@@ -28,11 +33,14 @@ export TEMP=$(pwd | sed s/cre-genomes.*//)
 
 #The following shouldn't need to be changed, but should set automatically
 path1=$(pwd | sed s/data.*/data/)
+path2=$(pwd | sed s/data.*/scripts/)
 species=$(pwd | sed s/^.*\\/data\\/// | sed s/\\/.*//)
 genotype=$(pwd | sed s/.*\\/${species}\\/// | sed s/\\/.*//)
 sample=$(pwd | sed s/.*\\/${genotype}\\/// | sed s/\\/.*//)
 version=$(ls ${genotype}-v*.fa | sed s/.*\-v// | sed s/.fa//) 
-path2=""
+path3="exonerate"
+output1="protein2genome.output"
+output2="protein2genome.gff"
 
 #Look for fasta file, there can only be one!
 if ls *.fa >/dev/null 2>&1
@@ -51,27 +59,40 @@ else
 	echo "No fasta file found, please check and restart"
 fi
 
-#Run EDTA
-if [ something ]
+#Make output directory
+if [ -d ${path3} ]
 then
+	cd ${path3}
+else
+	mkdir ${path3}
+	cd ${path3}
+fi
+
+#Run EDTA
+if [ -s ${output2} ]
+then
+	echo "${output2} already exists."
+	echo "To rerun this step, delete ${output2} and resubmit"
+else
+	echo "Running exonerate protein2genome on ${fasta}"
 	exonerate \
 		--cores ${threads} \
 		--model protein2genome \
-		--bestn 5 \
-		--minintron 10 \
-		--maxintron 3000 \
+		--bestn ${bestn} \
+		--minintron ${minintron} \
+		--maxintron ${maxintron} \
 		--query ${path1}/${protein} \
-		--target ${fasta} \
+		--target ../${fasta} \
 		--showtargetgff yes \
 		--showalignment no \
 		--showvulgar no \
-		--ryo ${ryo} > ${output}
-
-		--refine
-
-	echo "Converting gtf to gff3"
-	gffread -E protein_exonerate.gtf -o- > protein_exonerate.gff
+		--ryo ${ryo} \
+		--refine > ${output1}
+	#Reformat exonerate output
+	echo "Reformatting exonerate gff"
+	perl ${path2}/annotation/reformat_exonerate_protein_gff.pl \
+		--input_gff ${output1} \
+		--output_gff ${output2}
 fi
-
 
 echo "Done"
