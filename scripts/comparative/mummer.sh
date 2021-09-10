127406ad7ce8a3e8c49209329383e4971f4d5783
Niederhuth <niederhu@msu.edu>
2021-09-09 22:44:15 -0400

    stuff

--- scripts/comparative/mummer.sh
+++ scripts/comparative/mummer.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=80GB
+#SBATCH --mem=100GB
 #SBATCH --job-name align-genomes
 #SBATCH --output=job_reports/%x-%j.SLURMout
 
@@ -12,14 +12,14 @@ conda="${HOME}/miniconda3"
 
 #Set variables
 threads=20
-fa1=
-fa2=
+fa1=../S1/ref/S1-vflye.fa
+fa2=../:L1/ref/L1-vflye.fa
 
 #Change to current directory
 cd ${PBS_O_WORKDIR}
 #Export paths to conda
-export PATH="${conda}/envs/test/bin:${PATH}"
-export LD_LIBRARY_PATH="${conda}/envs/test/lib:${LD_LIBRARY_PATH}"
+export PATH="${conda}/envs/scaffolding/bin:${PATH}"
+export LD_LIBRARY_PATH="${conda}/envs/scaffolding/lib:${LD_LIBRARY_PATH}"
 
 #Align Genomes
 echo "Running nucmer"
