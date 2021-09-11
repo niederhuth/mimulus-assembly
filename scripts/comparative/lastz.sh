346558c46bcd8174b6c14723a8a469213fb4ad10
Niederhuth <niederhu@msu.edu>
2021-09-10 21:32:52 -0400

    stuff

--- scripts/comparative/lastz.sh
+++ scripts/comparative/lastz.sh
@@ -1,28 +1,33 @@
 #!/bin/bash --login
-#SBATCH --time=96:00:00
+#SBATCH --time=168:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=200GB
-#SBATCH --job-name align-genomes
+#SBATCH --job-name nucmer
 #SBATCH --output=job_reports/%x-%j.SLURMout
 
 #Set this variable to the path to wherever you have conda installed
 conda="${HOME}/miniconda3"
 
 #Set variables
-threads=1
-fa1=../S1/ref/S1-vflye.fa
-fa2=../L1/ref/L1-vflye.fa
+threads=10
+fa1=scaffold_507.fa
+fa2=contig_1056.fa
 
 #Change to current directory
 cd ${PBS_O_WORKDIR}
 #Export paths to conda
-export PATH="${conda}/envs/scaffolding/bin:${PATH}"
-export LD_LIBRARY_PATH="${conda}/envs/scaffolding/lib:${LD_LIBRARY_PATH}"
+export PATH="${conda}/envs/mummer/bin:${PATH}"
+export LD_LIBRARY_PATH="${conda}/envs/mummer/lib:${LD_LIBRARY_PATH}"
 
-#Align Genomes
-echo "Running nucmer"
-nucmer ${fa1} ${fa2}
+#Align Sequences
+lastz \
+	${fa1} \
+	${fa2} \
+	--strand=both \
+	--inner=1000 \
+	--format=maf \
+	--output=${fa2}_${fa1}_lastz
 
 echo "Done"
