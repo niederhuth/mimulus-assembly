f98555a92d522b077cbaa9c5156d72a8b302167f
Niederhuth <niederhu@msu.edu>
2021-10-03 14:47:56 -0400

    stuff

--- scripts/assembly/tigmint_long.sh
+++ scripts/assembly/tigmint_long.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=500GB
 #SBATCH --job-name tigmint-long
-#SBATCH --output=../../../job_reports/%x-%j.SLURMout
+#SBATCH --output=../../job_reports/%x-%j.SLURMout
 
 #Set this variable to the path to wherever you have conda installed
 conda="${HOME}/miniconda3"
@@ -35,7 +35,6 @@ sample=$(pwd | sed s/.*\\/${species}\\/${genotype}\\/// | sed s/\\/.*//)
 condition="assembly"
 assembly=$(pwd | sed s/^.*\\///)
 path2=$(pwd | sed s/${genotype}\\/${sample}.*/${genotype}\\/${sample}/)
-path3="tigmint"
 
 #Get genome size estimate
 genomeSize=$(awk -v FS="," \
@@ -48,67 +47,84 @@ genomeSize=$(awk -v FS="," \
 	${path1}/samples.csv)
 genomeSize2=$(python -c "print(${genomeSize/g/} * 1000000000)")
 
-#Look for fasta file, there can only be one!
-if [ -z ${input} ]
-then
-	echo "No input fasta provided, looking for fasta"
-	if ls *.fa >/dev/null 2>&1
+for i in 
+do
+
+	#set directory name
+	if [ -z ${input_dir} ]
 	then
-		input=$(ls *fa | sed s/.*\ //)
-		name=${input/.fa/}
-		echo "Fasta file ${input} found"
-	elif ls *.fasta >/dev/null 2>&1
-	then
-		input=$(ls *fasta | sed s/.*\ //)
-		name=${input/.fasta/}
-		echo "Fasta file ${input} found"
-	elif ls *.fna >/dev/null 2>&1
-	then
-		input=$(ls *fna | sed s/.*\ //)
-		name=${input/.fna/}
-		echo "Fasta file ${input} found"
+		path3="tigmint_long"
+		path4="../${i}"
 	else
-		echo "No fasta file found, please check and restart"
+		path3="tigmint_long_${i}"
+		path4=".."
 	fi
-else
-	echo "Input fasta: ${input}"
+
+	#Make and cd to output directory
+	if [ -d ${path3} ]
+	then
+		cd ${path3}
+	else
+		mkdir ${path3}
+		cd ${path3}
+	fi
+
+	#Look for fasta file, there can only be one!
+	if [ -z ${input} ] && [ ${input_dir} ]
+	then
+		echo "No input fasta provided, looking for fasta"
+		if ls ${path4}/*.fa >/dev/null 2>&1
+		then
+			input=$(ls ${path4}/*fa | sed s/.*\\///)
+			name=${input/.fa/}
+			echo "Fasta file ${input} found"
+		elif ls ${path4}/*.fasta >/dev/null 2>&1
+		then
+			input=$(ls ${path4}/*fasta | sed s/.*\\///)
+			name=${input/.fasta/}
+			echo "Fasta file ${input} found"
+		elif ls ${path4}/*.fna >/dev/null 2>&1
+		then
+			input=$(ls ${path4}/*fna | sed s/.*\\///)
+			name=${input/.fna/}
+			echo "Fasta file ${input} found"
+		else
+			echo "No fasta file found, please check and restart"
+		fi
+	else
+		echo "Input fasta: ${input}"
+	fi
+
+	#Copy and rename files...because of stupid eccentricities of some code
+	cp ${path4}/${input} ${name}.fa
+	cp ${path2}/fastq/${datatype}/clean.fastq.gz reads.fq.gz
+
+	#Run tigmint
+	echo "Running tigmint-long"
+	tigmint-make tigmint-long \
+		draft=${path4}/${name} \
+		reads=reads \
+		longmap=${datatype} \
+		cut=${cut} \
+		span=${span} \
+		dist=${dist} \
+		window=${window} \
+		minsize=${minsize} \
+		trim=${trim} \
+		G=${genomeSize2/.0/} \
+		t=${threads}
+
+	#Clean some stuff up for downstream analyses
+	unlink ${name}.cut${cut}.tigmint.fa
+	rm ${name}.fa ${name}.fa.fai reads.fq.gz
+
+	#rename fasta & bed file to something more easily handled
+	long_name=${name}.reads.cut${cut}.molecule.size${minsize}.trim${trim}.window${window}.span${span}.breaktigs
+	mv ${long_name}.fa ${name}_tigmint.fa
+	mv ${long_name}.bed ${name}_tigmint.bed
+
+	cd ../
+	echo "tigmint-long on ${i} complete"
 fi
-
-#Make and cd to output directory
-if [ -d ${path3} ]
-then
-	cd ${path3}
-else
-	mkdir ${path3}
-	cd ${path3}
-fi
-
-#Copy and rename files...because of stupid eccentricities of some code
-cp ../${input} ${name}.fa
-cp ${path2}/fastq/${datatype}/clean.fastq.gz reads.fq.gz
-
-#Run tigmint
-echo "Running tigmint-long"
-tigmint-make tigmint-long \
-	draft=${name} \
-	reads=reads \
-	longmap=${datatype} \
-	cut=${cut} \
-	span=${span} \
-	dist=${dist} \
-	window=${window} \
-	minsize=${minsize} \
-	trim=${trim} \
-	G=${genomeSize2/.0/} \
-	t=${threads}
-
-#Clean some stuff up for downstream analyses
-unlink ${name}.cut${cut}.tigmint.fa
-rm ${name}.fa ${name}.fa.fai reads.fq.gz
-
-#rename fasta & bed file to something more easily handled
-long_name=${name}.reads.cut${cut}.molecule.size${minsize}.trim${trim}.window${window}.span${span}.breaktigs
-mv ${long_name}.fa ${name}_tigmint.fa
-mv ${long_name}.bed ${name}_tigmint.bed
 
 echo "Done"
