57778a5ca4eaf7917d48b6e60711781748decbd4
Niederhuth <niederhu@msu.edu>
2021-10-09 14:31:40 -0400

    stuff

--- scripts/assembly/sort_rename_fa.sh
+++ scripts/assembly/sort_rename_fa.sh
@@ -6,7 +6,7 @@
 cat $1 |\
 awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' |\
 awk -F '\t' '{printf("%d\t%s\n",length($2),$0);}' |\
-awk -v a=$2 '$1 > a' |\
+awk -v a=$2 '$1 >= a' |\
 sort -k1,1rn |\
 cut -f 2- |\
 tr "\t" "\n" |\
