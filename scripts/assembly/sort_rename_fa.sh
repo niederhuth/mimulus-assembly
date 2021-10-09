f38a70f59723fd933fcd699faea9c46453a22582
Niederhuth <niederhu@msu.edu>
2021-10-09 13:01:28 -0400

    stuff

---
+++ scripts/assembly/sort_rename_fa.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Ugly script to sort and rename fasta files
+
+cat $1 |\
+awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' |\
+awk -F '\t' '{printf("%d\t%s\n",length($2),$0);}' |\
+sort -k1,1rn |\
+cut -f 2- |\
+tr "\t" "\n" |\
+awk -v i=1000 '/^>/{print ">contig" ++i, $0; next}{print}' |\
+sed s/\ \>.*\:\:/\ /
+
+
