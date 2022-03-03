b14b3e4dee82f35dab4d142dd91f97db84f1c0b3
Chad Niederhuth <niederhu@msu.com>
2022-03-02 23:26:04 -0500

    stuff

--- scripts/dev/tgsgapcloser.sh
+++ scripts/dev/tgsgapcloser.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=500GB
-#SBATCH --job-name tigmint-long
+#SBATCH --job-name tgsgapcloser
 #SBATCH --output=../../job_reports/%x-%j.SLURMout
 
 #Set this variable to the path to wherever you have conda installed
@@ -13,10 +13,10 @@ conda="${HOME}/miniconda3"
 #Set variables
 threads=20 #doesn't seem to want to use more than 6
 datatype="ont" #ont or pb
-
-
+racon=FALSE
+pilon=FALSE
+ngs=
 input= #input fasta, if left blank, will look for it in current directory, mutually exclusive with input_dir
-input_dir="pilon" #common directory name, e.g. pilon or polca to look for assemblies, eclusive with "input"
 
 #Change to current directory
 cd ${PBS_O_WORKDIR}
@@ -32,6 +32,7 @@ sample=$(pwd | sed s/.*\\/${species}\\/${genotype}\\/// | sed s/\\/.*//)
 condition="assembly"
 assembly=$(pwd | sed s/^.*\\///)
 path2=$(pwd | sed s/${genotype}\\/${sample}.*/${genotype}\\/${sample}/)
+reads="${path2}/fastq/${datatype}/clean.fastq.gz"
 
 #Look for fasta file, there can only be one!
 if [ -z ${input} ]
@@ -60,11 +61,11 @@ fi
 options="--thread ${threads} --tgstype ${datatype}"
 if [ ${racon} = "TRUE" ]
 then
-	options="${options} --racon"
+	options="${options} --racon ${conda}/envs/test/bin/racon"
 fi
 if [ ${pilon} = "TRUE" ]
 then
-	options="${options} --ngs ${ngs} --pilon"
+	options="${options} --ngs ${ngs} --pilon ${conda}/envs/test/bin/pilon --samtools ${conda}/envs/test/bin/samtools"
 fi
 if [[ ${racon} = "FALSE" && ${pilon} = "FALSE" ]]
 	options="--ne"
@@ -74,31 +75,23 @@ fi
 echo "Running tgsgapcloser"
 tgsgapcloser \
 	--scaff ${input} \
-	--reads ${} \
+	--reads ${reads} \
 	--output tgsgapcloser \
 	${options}
 
 echo "Done"
 
-          --ne                             do not error correct. error correct by default.
-          or
-          --racon     <racon>              the installed racon.
-          or
-          --ngs       <ngs_reads>          the ngs reads used for pilon
-          --pilon     <pilon>              the installed pilon.
-          --samtools  <samtools>           the installed samtools.
-          --java      <java>               the installed java.
-      optional:
-          --min_idy   <min_idy>            min_idy for filter reads .
-                                           0.3 for ont by default.
-                                           0.2 for pb by default.
-          --min_match <min_idy>            min match length for filter reads .
-                                           300bp for ont by default.
-                                           200bp for pb by default.
-          --chunk     <chunk_num>          split candidate into chunks to error-correct separately.
-          --pilon_mem <t_num>              memory used for pilon , 300G for default.
-          --p_round   <pilon_round>        pilon error-correction round num . 3 by default.
-          --r_round   <racon_round>        racon error-correction round num . 1 by default.
-          --g_check                        gapsize diff check , none by default.
+#          --java      <java>               the installed java.
+#      optional:
+#          --min_idy   <min_idy>            min_idy for filter reads .
+#                                           0.3 for ont by default.
+#                                           0.2 for pb by default.
+#         --min_match <min_idy>            min match length for filter reads .
+#                                           300bp for ont by default.
+#                                           200bp for pb by default.
+#          --chunk     <chunk_num>          split candidate into chunks to error-correct separately.
+#          --pilon_mem <t_num>              memory used for pilon , 300G for default.
+#          --p_round   <pilon_round>        pilon error-correction round num . 3 by default.
+#          --r_round   <racon_round>        racon error-correction round num . 1 by default.
+#          --g_check                        gapsize diff check , none by default.
 
-echo "Done"
