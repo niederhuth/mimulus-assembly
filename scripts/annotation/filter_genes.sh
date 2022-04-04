a2419ff922016c515cbd4e8c4b18b103659e3b93
Chad Niederhuth <niederhu@msu.com>
2022-04-04 15:16:49 -0400

    stuff

--- scripts/annotation/filter_genes.sh
+++ scripts/annotation/filter_genes.sh
@@ -70,14 +70,31 @@ export TMP=$(pwd)
 export TEMP=$(pwd)
 
 #Set various datasets
-transcripts=${maker_dir}/maker_round2_transcripts.fa
-proteins=${maker_dir}/maker_round2_proteins.fa
-gff=${maker_dir}/maker_round2.gff
+transcripts=${maker_dir}/maker_transcripts.fa
+proteins=${maker_dir}/maker_proteins.fa
+gff=${maker_dir}/maker.gff
+gene_ids=maker_standard_gene_list.txt
 
-#
+#Check for Pfam A
+if [[ -z Pfam-A.hmm || -z Pfam-A.hmm.gz ]]
+then
+	echo Pfam-A.hmm found
+	if [ -z Pfam-A.hmm.gz ]
+	then
+		gunzip Pfam-A.hmm.gz
+	fi
+else
+	echo "Downloading Pfam-A"
+	wget http://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz
+	gunzip Pfam-A.hmm.gz
+fi
+
+#Prepare Pfam A hmm-database
+echo "Preparing Pfam A hmm database"
 hmmpress Pfam-A.hmm
 
-#
+#Search Pfam A hmm domains
+echo "Searching against Pfam A hmm profiles"
 hmmscan \
 	--domE 1e-5 \
 	-E 1e-5 \
@@ -86,35 +103,35 @@ hmmscan \
 	Pfam-A.hmm \
 	${proteins}
 
-#Generate starndard gene list
-echo ""
+#Generate maker standard gene list
+echo "Generating Maker Standard Gene List"
 perl ${path2}/annotation/generate_maker_standard_gene_list.pl \
 	--input_gff ${gff} \
 	--pfam_results prot_domains.out \
 	--pfam_cutoff 1e-10 \
 	--output_file maker_standard_gene_list.txt
 
-#
-gene_ids=maker_standard_gene_list.txt
-echo ""
-perl get_subset_of_fastas.pl \
+#Make Maker Standard Files
+echo "Making Maker Standard Transcripts fasta"
+perl ${path2}/annotation/get_subset_of_fastas.pl \
 	-l ${gene_ids} \
 	-f ${transcripts} \
 	-o maker_standard_transcripts.fa
 	
-echo ""
-perl get_subset_of_fastas.pl \
+echo "Making Maker Standard Protein fasta"
+perl ${path2}/annotation/get_subset_of_fastas.pl \
 	-l ${gene_ids} \
 	-f ${proteins} \
 	-o maker_standard_proteins.fa
 
-#
-perl /data/run/miughetta/scripts/create_maker_standard_gff.pl \
+echo "Making Maker Standard GFF file"
+perl ${path2}/annotation/create_maker_standard_gff.pl \
 	--input_gff ${gff} \
 	--output_gff maker_standard.gff3 \
-	--maker_standard_gene_list maker_standard_gene_list.txt
+	--maker_standard_gene_list ${gene_ids}
 
-#
+#Make Transposase BLAST DB
+echo "Making Transposase BLAST DB"
 makeblastdb \
 	-in Tpases020812 \
 	-title "Tpases020812DB" \
@@ -122,6 +139,9 @@ makeblastdb \
 	-out Tpases020812DB \
 	-dbtype prot
 
+
+#Run BLAST against Transposases 
+echo "Running BLAST on Transposases"
 blastp \
 	-db Tpases020812DB \
 	-query ${proteins} \
@@ -130,13 +150,22 @@ blastp \
 	-outfmt 6 \
 	-num_threads ${threads}
 
-#
-scp -P 55533 /Users/Mimi/Documents/College/MSU/GyDB_collection.zip
-unzip GyDB_collection.zip
-cat *hmm > all_gypsy.hmm
+#Download Gypsy DB hmm files and format the hmm database
+if [ -z all_gypsy.hmm ]
+then
+	echo "all_gypsy.hmm found"
+else
+	echo "Downloading GyDB_collection"
+	wget https://gydb.org/extensions/Collection/collection/db/GyDB_collection.zip
+	unzip GyDB_collection.zip
+	echo "Combining GyDB hmm profiles"
+	cat GyDB_collection/profiles/*hmm > all_gypsy.hmm
+fi
+echo "Formatting all_gypsy.hmm database"
 hmmpress all_gypsy.hmm
 
-#
+#Search gypsy hmm profiles
+echo "Searching against gypsy hmm profiles"
 hmmscan \
 	--domE 1e-5 \
 	-E 1e-5 \
@@ -146,8 +175,7 @@ hmmscan \
 	${proteins}
 
 #
-mv create_no_TE_genelist.py create_no_TE_genelist_noRef.py
-python create_no_TE_genelist_noRef.py \
+python ${path2}/annotation/create_no_TE_genelist.py \
 	--input_file_TEpfam TE_Pfam_domains.txt \
 	--input_file_maxPfam Sreb_prot_domains.out \
 	--input_file_geneList_toKeep Sreb_maker2_standard_gene_list.txt \
@@ -161,17 +189,17 @@ transcripts=Sreb_maker_standard_transcripts.fasta
 proteins=Sreb_maker_standard_proteins.fasta
 input_gff=Sred_maker2_standard.gff3
 
-perl get_subset_of_fastas.pl \
+perl ${path2}/annotation/get_subset_of_fastas.pl \
 	-l $gene_ids \
 	-f $transcripts \
 	-o Sreb_v1.0_maker_std_transcripts_noTE.fasta
 
-perl get_subset_of_fastas.pl \
+perl ${path2}/annotation/get_subset_of_fastas.pl \
 	-l $gene_ids \
 	-f $proteins \
 	-o Sreb_v1.0_maker_std_proteins_noTE.fasta
 
-perl create_maker_standard_gff.pl \
+perl ${path2}/annotation/create_maker_standard_gff.pl \
 	--input_gff $input_gff \
 	--output_gff Sreb_v1.0_maker_std_noTE.gff3 \
 	--maker_standard_gene_list noTE_Sreb_v1.0_maker2standard_gene_list.txt
