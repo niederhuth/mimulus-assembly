92ec9ea3576d1026f554635ba6a084a77d4481a4
Niederhuth <niederhu@msu.edu>
2022-06-14 15:06:25 -0400

    stuff

---
+++ scripts/annotation/rename_annotations_by_chr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash --login
+#SBATCH --time=3:59:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1GB
+#SBATCH --job-name=rename_annotations_by_chr
+#SBATCH --output=../job_reports/%x-%j.SLURMout
+
+#
+
+
+#
+
+#
