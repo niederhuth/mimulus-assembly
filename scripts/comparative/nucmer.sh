94e2f4131572e99c028ad9e76f5972d7ce83ff80
Niederhuth <niederhu@msu.edu>
2022-09-09 14:29:21 -0400

    stuff

--- scripts/comparative/nucmer.sh
+++ scripts/comparative/nucmer.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=200GB
 #SBATCH --job-name nucmer
-#SBATCH --output=../job_reports/%x-%j.SLURMout
+#SBATCH --output=job_reports/%x-%j.SLURMout
 
 #Set this variable to the path to wherever you have conda installed
 conda="${HOME}/miniconda3"
