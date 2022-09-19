d3a8c21e7e1eb0f80e32a812f3387bbd7a3b4958
Chad Niederhuth <niederhu@msu.com>
2022-09-18 23:14:21 -0400

    stuff

--- scripts/dev/liftoff.sh
+++ scripts/dev/liftoff.sh
@@ -1,9 +1,9 @@
 #!/bin/bash --login
-#SBATCH --time=168:00:00
+#SBATCH --time=3:59:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=500GB
+#SBATCH --mem=20GB
 #SBATCH --job-name liftoff
 #SBATCH --output=../job_reports/%x-%j.SLURMout
 
@@ -11,15 +11,11 @@
 conda="${HOME}/miniconda3"
 
 #Set variables
-gene_list=""
-chroms="../chroms.txt"
-target_fa="../S1-v1.fa"
-ref_fa="../L1-v1.fa"
-target_gff="../S1-v1.gff"
-ref_gff="../L1-v1.gff"
+target_fa= #target genome fasta to map gff files to, if left blank, look in current directory
+target_gff= #gff file for target genome, only necessary if gffcompare=TRUE
 
 #Change to current directory
-cd ${PBS_O_WORKDIR}
+#cd ${PBS_O_WORKDIR}
 #Export paths to conda
 export PATH="${conda}/envs/liftoff/bin:${PATH}"
 export LD_LIBRARY_PATH="${conda}/envs/liftoff/lib:${LD_LIBRARY_PATH}"
@@ -29,65 +25,162 @@ export TMPDIR=$(pwd | sed s/data.*/data/)
 export TMP=$(pwd | sed s/data.*/data/)
 export TEMP=$(pwd | sed s/data.*/data/)
 
-#Subset gff
-if [ ${gene_list} ]
+#The following shouldn't need to be changed, but should set automatically
+path1=$(pwd | sed s/data.*/misc/)
+species=$(pwd | sed s/^.*\\/data\\/// | sed s/\\/.*//)
+genotype=$(pwd | sed s/.*\\/${species}\\/// | sed s/\\/.*//)
+sample=$(pwd | sed s/.*\\/${species}\\/${genotype}\\/// | sed s/\\/.*//)
+condition="annotation"
+datatype="liftoff"
+path2=$(pwd | sed s/data.*/data/)
+path3="liftoff"
+
+#Look for fasta file, there can only be one!
+if [ -z ${target_fa} ]
 then
-	fgrep -f ${gene_list} ${ref_gff} > search.gff
+	echo "No input fasta provided, looking for fasta"
+	if ls *.fa >/dev/null 2>&1
+	then
+		fasta=$(ls *fa | sed s/.*\ //)
+		echo "Fasta file ${target_fa} found"
+	elif ls *.fasta >/dev/null 2>&1
+	then
+		fasta=$(ls *fasta | sed s/.*\ //)
+		echo "Fasta file ${target_fa} found"
+	elif ls *.fna >/dev/null 2>&1
+	then
+		fasta=$(ls *fna | sed s/.*\ //)
+		echo "Fasta file ${target_fa} found"
+	else
+		echo "No fasta file found, please check and restart"
+	fi
 else
-	cat ${ref_gff} > search.gff
+	echo "Input fasta: ${target_fa}"
 fi
 
-#Run liftoff
-liftoff \
-	-cds \
-	-polish \
-	-o mapped.gff \
-	-g search.gff \
-	-chroms ${chroms} \
-	${target_fa} \
-	${ref_fa}
+#Check for and make/cd working directory
+if [ -d ${path3} ]
+then
+	cd ${path3}
+else
+	mkdir ${path3}
+	cd ${path3}
+fi
 
-#Get genes with a valid ORF otherwise classify as pseudogenes
-grep valid_ORFs=1 mapped.gff_polished | cut -f9 | sed 's/\;.*//' | sed 's/ID\=//' > validgenes
-grep valid_ORFs=0 mapped.gff_polished | cut -f9 | sed 's/\;.*//' | sed 's/ID\=//' > pseudogenes
+#Get list of genomes
+genomes=$(awk -v FS="," \
+	-v a=${species} \
+	-v b=${genotype} \
+	-v c=${sample} \
+	-v d=${condition} \
+	-v e=${datatype} \
+	'{if ($1 == a && $2 == b && $3 == c && $4 == d && $5 == e) print $7}' \
+	${path1}/samples.csv)
 
-#Subset gff of valid genes
-fgrep -f validgenes mapped.gff_polished > validgenes.gff
+#Loop over genomes and liftover & process the annotations
+for i in ${genomes}
+do
+	mkdir ${i}
+	ref_fa=$(ls ${path2}/${i/_*/}/${i/*_/}/ref/${i/*_/}-v*.fa)
+	ref_gff=$(ls ${path2}/${i/_*/}/${i/*_/}/ref/annotations/${i/*_/}-v*.gff)
+	chroms=${path1}/annotations/${species}_${genotype}_${i}_chr_mapping.csv
 
-#Subset gff of pseudogenes
-fgrep -f pseudogenes mapped.gff_polished > pseudogenes.gff
+	#Run liftoff
+	mkdir liftoff
+	cd liftoff
+	echo "Lifting over annotations from ${i/_*/} ${i/*_/} to ${species} ${genotype} with liftoff"
+	liftoff \
+		-cds \
+		-polish \
+		-o mapped.gff \
+		-g ${ref_gff} \
+		-chroms ${chroms} \
+		${target_fa} \
+		${ref_fa}
+	cd ../
 
-#Classify annotations with gffcompare
-gffcompare -r ${target_gff} validgenes.gff
+	#Classify annotations with gffcompare
+	if [ ! -z ${target_gff} ]
+	then
+		echo "Running gffcompare"
+		mkdir gffcompare
+		cd gffcompare
+		gffcompare -r ${target_gff} ../liftoff/mapped.gff_polished
 
-#= - Discard - complete, exact match of intron chain
-#c - Prob Discard - contained in reference (intron compatible)
-#e - Prob Discard - single exon transfrag partially covering an intron, possible pre-mRNA fragment
-#i - Prop Keep - fully contained within a reference intron
-#j - Discard - multi-exon with at least one junction match
-#k - Prob Discard - containment of reference (reverse containment)
-#m - Prob Discard - retained intron(s), all introns matched or retained
-#n - Prob Discard - retained intron(s), not all introns matched/covered
-#o - Prob Discard - other same strand overlap with reference exons
-#p - Keep - possible polymerase run-on (no actual overlap)
-#s - Keep - intron match on the opposite strand (likely amapping error)
-#x - Keep - exonic overlap on the opposite strand (like o or e but on the opposite strand)
-#y - Keep - contains a reference within its intron(s)
-#u - Keep - none of the above (unknown, intergenic)
-#Keep p,s,u,x,y
-awk '$4=="p" || $4=="s" || $4 =="u" || $4=="x" || $4=="y"' gffcmp.tracking | \
-cut -f5 | sed 's/q1\://' | sed 's/|.*//' > newgenes
+		#= - Discard - complete, exact match of intron chain
+		#c - Prob Discard - contained in reference (intron compatible)
+		#e - Prob Discard - single exon transfrag partially covering an intron, possible pre-mRNA fragment
+		#i - Prop Keep - fully contained within a reference intron
+		#j - Discard - multi-exon with at least one junction match
+		#k - Prob Discard - containment of reference (reverse containment)
+		#m - Prob Discard - retained intron(s), all introns matched or retained
+		#n - Prob Discard - retained intron(s), not all introns matched/covered
+		#o - Prob Discard - other same strand overlap with reference exons
+		#p - Keep - possible polymerase run-on (no actual overlap)
+		#s - Keep - intron match on the opposite strand (likely amapping error)
+		#x - Keep - exonic overlap on the opposite strand (like o or e but on the opposite strand)
+		#y - Keep - contains a reference within its intron(s)
+		#u - Keep - none of the above (unknown, intergenic)
+		#Keep p,s,u,x,y
+		awk '$4=="p" || $4=="s" || $4 =="u" || $4=="x" || $4=="y"' gffcmp.tracking | \
+		cut -f5 | sed 's/q1\://' | sed 's/|.*//' > ../newgenes
+		cd ../
 
-#Subset gff to keep new_genes
-fgrep -f newgenes mapped.gff_polished | bedtools sort > newgenes.gff
+		#Subset gff to keep new_genes
+		echo "Subsetting the new annotations"
+		fgrep -f newgenes liftoff/mapped.gff_polished | bedtools sort > newgenes.gff
+	else
+		cp liftoff/mapped.gff_polished newgenes.gff
+	fi
 
-#
-cat ${target_gff} newgenes.gff | bedtools sort > combined.gff
+	#Get genes with a valid ORF otherwise classify as new_pseudogenes
+	grep valid_ORFs=1 newgenes.gff | cut -f9 | sed 's/\;.*//' | sed 's/ID\=//' > new_valid_genes
+	grep valid_ORFs=0 newgenes.gff | cut -f9 | sed 's/\;.*//' | sed 's/ID\=//' > new_pseudogenes
+	echo "$(wc -l new_valid_genes) putative genes with intact ORFs found"
+	echo "$(wc -l new_pseudogenes) putative new_pseudogenes found"
 
-#
-awk '$3=="gene"' combined.gff | cut -f1,2,4,5,9 | sed 's/ID\=//' | sed 's/\;.*//' > combined_order
+	#Subset gff of valid genes
+	fgrep -f new_valid_genes newgenes.gff > new_valid_genes.gff
 
+	#Subset gff of new_pseudogenes
+	#Change column 3 from gene to pseudogene
+	#Add attribute pseudo_gene=TRUE
+	fgrep -f new_pseudogenes newgenes.gff | \
+	awk -v OFS="\t" '{if ($3=="gene") print $1,$2,"pseudogene",$4,$5,$6,$7,$8,$9";putative_pseudogene=TRUE"; 
+					else print$0";putative_pseudogene=TRUE"}' > new_pseudogenes.gff
 
+	#Combine the gff files
+	if [ ! -z ${target_gff} ]
+	then
+		cat ${target_gff} newgenes.gff | bedtools sort > all_annot.gff
+		cat ${target_gff} new_valid_genes.gff | bedtools sort > all_valid.gff
+		cat ${final_pseudo} new_pseudogenes.gff | bedtools sort > all_pseudogenes.gff
+	else
+		cat newgenes.gff | bedtools sort > all_annot.gff
+		cat new_valid_genes.gff | bedtools sort > all_valid.gff
+		cat new_pseudogenes.gff | bedtools sort > all_pseudogenes.gff
+	fi
+
+	#Get the order of genes for renaming
+	awk '$3=="gene"' all_valid.gff | cut -f1,2,4,5,9 | sed 's/ID\=//' | sed 's/\;.*//' > valid_gene_order.tsv
+
+	#Change the target_gff
+	target_gff="$(pwd)/all_annot.gff"
+	#Set final outputs
+	final_valid="$(pwd)/all_valid.gff"
+	final_order="$(pwd)/valid_gene_order.tsv"
+	final_pseudo="$(pwd)/all_pseudogenes.gff"
+
+	#Change out of the directory
+	cd ../
+done
+
+#Copy the final files
+cp ${final_valid} final_valid_genes.gff
+cp ${final_oder} final_valid_gene_order.gff
+cp ${final_pseudo} final_pseudogenes.gff
+
+echo "Done"
 
 
 
--- scripts/dev/renaming.R
+++ scripts/dev/renaming.R
@@ -4,16 +4,36 @@ df <- read.table("combined_order")
 colnames(df) <- c("chr","source","start","stop","gene")
 
 df$up_pos <- NA
+new_count=10
 for(i in 1:nrow(df)){
 	if(df[i,]$source == "maker"){
 		maker_pos <- df[i,]$gene
 	} else if(i == 1 & df[i,]$source == "Liftoff"){
-
-		df[i,]$up_pos <- maker_pos <- paste("Mg",gsub("Mg","",gsub("_.*","",head(df[df$source=="maker",]$gene,1))),
-			"_",gsub("01","1",gsub("chr","0",df[i,]$chr)),"g","00000",sep="")
+		if(nrow(head(df[df$chr == df[i,]$chr & df$source == "maker",],1)) == 1){
+			pos_num=as.numeric(gsub(".*g","",head(df[df$chr == df[i,]$chr & df$source == "maker",],1)$gene))-10
+			pos_num=paste(paste(rep(0,5-nchar(pos_num)),collapse=""),pos_num,sep="")
+			df[i,]$up_pos <- maker_pos <- paste("Mg",gsub("Mg","",gsub("_.*","",head(df[df$source=="maker",]$gene,1))),
+				"_",gsub("contig_.*","UN",gsub("01","1",gsub("chr","0",df[i,]$chr))),"g",pos_num,sep="")
+		} else if(nrow(head(df[df$chr == df[i,]$chr & df$source == "maker",],1)) == 0){
+			pos_num=as.numeric(gsub(".*g","",tail(df[df$source=="maker",],1)$gene))+new_count
+			pos_num=paste(paste(rep(0,5-nchar(pos_num)),collapse=""),pos_num,sep="")
+			df[i,]$up_pos <- maker_pos <- paste("Mg",gsub("Mg","",gsub("_.*","",head(df[df$source=="maker",]$gene,1))),
+				"_",gsub("contig_.*","UN",gsub("01","1",gsub("chr","0",df[i,]$chr))),"g",pos_num,sep="")
+			new_count=new_count+10
+		}
 	} else if(df[i,]$chr != df[i-1,]$chr & df[i,]$source == "Liftoff"){
-		df[i,]$up_pos <- maker_pos <- paste("Mg",gsub("Mg","",gsub("_.*","",head(df[df$source=="maker",]$gene,1))),
-			"_",gsub("01","1",gsub("chr","0",df[i,]$chr)),"g","00000",sep="")
+		if(nrow(head(df[df$chr == df[i,]$chr & df$source == "maker",],1)) == 1){
+			pos_num=as.numeric(gsub(".*g","",head(df[df$chr == df[i,]$chr & df$source == "maker",],1)$gene))-10
+			pos_num=paste(paste(rep(0,5-nchar(pos_num)),collapse=""),pos_num,sep="")
+			df[i,]$up_pos <- maker_pos <- paste("Mg",gsub("Mg","",gsub("_.*","",head(df[df$source=="maker",]$gene,1))),
+				"_",gsub("contig_.*","UN",gsub("01","1",gsub("chr","0",df[i,]$chr))),"g",pos_num,sep="")
+		} else if(nrow(head(df[df$chr == df[i,]$chr & df$source == "maker",],1)) == 0){
+			pos_num=as.numeric(gsub(".*g","",tail(df[df$source=="maker",],1)$gene))+new_count
+			pos_num=paste(paste(rep(0,5-nchar(pos_num)),collapse=""),pos_num,sep="")
+			df[i,]$up_pos <- maker_pos <- paste("Mg",gsub("Mg","",gsub("_.*","",head(df[df$source=="maker",]$gene,1))),
+				"_",gsub("contig_.*","UN",gsub("01","1",gsub("chr","0",df[i,]$chr))),"g",pos_num,sep="")
+			new_count=new_count+10
+		}
 	} else if(df[i,]$source == "Liftoff") {
 		df[i,]$up_pos <- maker_pos
 	}
@@ -51,23 +71,32 @@ df2$new_pos <- df2$center_gene <- df2$region_center <- NA
 df3 <- unique(df2[,c("chr","up_pos")])
 for(i in 1:nrow(df3)){
 	tmp1 <- subset(df2, chr == df3[i,1] & up_pos == df3[i,2])
+	#If only one new gene is added between two existing genes
 	if(nrow(tmp1) == 1){
+		#if the gene overlaps with both the upstream & downstream gene, it is assigned a 5
 		if(tmp1[1,]$overlap_up == TRUE & tmp1[1,]$overlap_down == TRUE & tmp1[1,]$overlap_up_gene != tmp1[1,]$overlap_down_gene){
 			df2[df2$gene == tmp1[1,]$gene,]$new_pos <- gsub("0$","5",tmp1[1,]$up_pos)
+		#if the gene overlaps with the upstream gene and not the downstream gene, it is assigned a 2
 		} else if(tmp1[1,]$overlap_up == TRUE & tmp1[1,]$overlap_down == FALSE) { 
 			df2[df2$gene == tmp1[1,]$gene,]$new_pos <- gsub("0$","2",tmp1[1,]$up_pos)
+		#if the gene overlaps with the downstream gene and not the upstream gene, it is assigned an 8
 		} else if(tmp1[1,]$overlap_up == FALSE & tmp1[1,]$overlap_down == TRUE) { 
 			df2[df2$gene == tmp1[1,]$gene,]$new_pos <- gsub("0$","8",tmp1[1,]$up_pos)
+		#if the gene doesn't overlap with either, then it is assigned a 5
 		} else if(tmp1[1,]$overlap_up == FALSE & tmp1[1,]$overlap_down == FALSE) {
 			df2[df2$gene == tmp1[1,]$gene,]$new_pos <- gsub("0$","5",tmp1[1,]$up_pos)
 		}
-	} else if(nrow(tmp1) > 1){
+	#If there is more than one gene in the region
+	} else if(nrow(tmp1) > 1 & nrow(tmp1) <= 9){
+		#Get the center point for each new gene
 		tmp1$gene_center <- NA
 		for(x in 1:nrow(tmp1)){
 			tmp1[x,]$gene_center <- mean(c(tmp1[x,]$start,tmp1[x,]$stop))
 		}
+		#Calculate the center position & range for the region
 		region_center <- mean(c(df[as.numeric(row.names(tmp1[1,]))-1,]$stop,df[as.numeric(row.names(tmp1[nrow(tmp1),]))+1,]$start))
 		region_range <- region_center - df[as.numeric(row.names(tmp1[1,]))-1,]$stop
+		#Identify the central most gene
 		tmp1$center_gene <- NA
 		if(nrow(subset(tmp1,start < region_center & stop > region_center)) == 1){
 			tmp1$center_gene <- subset(tmp1,start < region_center & stop > region_center)$gene
@@ -79,36 +108,48 @@ for(i in 1:nrow(df3)){
 			closest <- which.min(abs(tmp1$gene_center - region_center))
 			tmp1$center_gene <- tmp1[closest,]$gene
 		}
+		#Get the number of genes upstream & downstream of the center gene
 		up_genes <- tmp1[tmp1$start < tmp1[tmp1$gene == tmp1$center_gene,]$start,]
 		down_genes <- tmp1[tmp1$start > tmp1[tmp1$gene == tmp1$center_gene,]$start,]
 		range <- abs(tmp1[tmp1$gene == tmp1$center_gene,]$gene_center - region_center) > region_range/4
+		#If there are more than 4 genes upstream and less than 4 downstream
 		if(nrow(up_genes) > 4 & nrow(down_genes) < 4){
+			#Adjust the position of the center gene accordingly
 			center_count <- nrow(up_genes) + 1
 			df2[df2$gene == tmp1[tmp1$gene==tmp1$center_gene,]$gene,]$new_pos <- gsub("0$",center_count,tmp1[tmp1$gene==tmp1$center_gene,]$up_pos)
 			df2[df2$gene == tmp1[tmp1$gene==tmp1$center_gene,]$gene,]$center_gene <- tmp1[1,]$center_gene
+		#If there are more than 4 genes downstream and less than 4 upstream
 		} else if(nrow(up_genes) < 4 & nrow(down_genes) > 4) {
+			#Adjust the position of the center gene accordingly
 			center_count <- 10 - nrow(down_genes) - 1
 			df2[df2$gene == tmp1[tmp1$gene==tmp1$center_gene,]$gene,]$new_pos <- gsub("0$",center_count,tmp1[tmp1$gene==tmp1$center_gene,]$up_pos)
 			df2[df2$gene == tmp1[tmp1$gene==tmp1$center_gene,]$gene,]$center_gene <- tmp1[1,]$center_gene
 		} else {
+			#Check how close to the center the center gene really is, if ts greater than a quarter then 1/4th the range then we adjust
 			if(abs(tmp1[tmp1$gene == tmp1$center_gene,]$gene_center - region_center) > region_range/4){
+				#If the difference is greater than zero, then its downstream of the center, so change the count to 6
 				if(tmp1[tmp1$gene == tmp1$center_gene,]$gene_center - region_center > 0){
 					center_count <- 6
+				#Otherwise its upstream, so change the count to 4
 				} else {
 					center_count <- 4
 				}
+			#Otherwise it stays as 5
 			} else {
 				center_count <- 5
 			}
+			#Set the name for the center gene
 			df2[df2$gene == tmp1[tmp1$gene==tmp1$center_gene,]$gene,]$new_pos <- gsub("0$",center_count,tmp1[tmp1$gene==tmp1$center_gene,]$up_pos)
 			df2[df2$gene == tmp1[x,]$gene,]$center_gene <- tmp1[1,]$center_gene
-				df2[df2$gene == tmp1[x,]$gene,]$region_center <- region_center
+			df2[df2$gene == tmp1[x,]$gene,]$region_center <- region_center
 		}
-		if((center_count - 1) > 3 & nrow(up_genes) == 3){
-			count=1
+		#Now we deal with the other genes
+		if((center_count - 1) == nrow(up_genes) || (center_count - 1) == ((floor((center_count - 1)/2)*nrow(up_genes)))){
+			count=0
 		} else {
 			count=1
 		}
+		#If the number of upstream genes is more than 1
 		if(nrow(up_genes) > 1){
 			for(x in 1:nrow(up_genes)){
 				count=count+floor((center_count - 1)/nrow(up_genes))
@@ -136,6 +177,14 @@ for(i in 1:nrow(df3)){
 			df2[df2$gene == down_genes[1,]$gene,]$new_pos <- gsub("0$",count,down_genes[1,]$up_pos)
 			df2[df2$gene == down_genes[1,]$gene,]$center_gene <- down_genes[1,]$center_gene
 			df2[df2$gene == down_genes[1,]$gene,]$region_center <- region_center 
+		}
+	} else {
+		print("Warning greater than 9 genes in region")
+		for(x in 1:nrow(tmp1)){
+			count=paste("x",x,sep="")
+			df2[df2$gene == tmp1[x,]$gene,]$new_pos <- gsub("0$",count,tmp1[x,]$up_pos)
+			df2[df2$gene == tmp1[x,]$gene,]$center_gene <- tmp1[1,]$center_gene
+			df2[df2$gene == tmp1[x,]$gene,]$region_center <- region_center
 		}
 	}
 }
