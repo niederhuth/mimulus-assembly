c37bba969e65ebc0cb95f742b4f22aa375c97019
Niederhuth <niederhu@msu.edu>
2022-12-01 12:17:09 -0500

    stuff

--- scripts/methylation/tombo_annotate_raw_with_fastqs.sh
+++ scripts/methylation/tombo_annotate_raw_with_fastqs.sh
@@ -2,8 +2,8 @@
 #SBATCH --time=168:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --mem=800GB
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=400GB
 #SBATCH --job-name tombo-annotate-raw-with-fastqs
 #SBATCH --output=job_reports/%x-%j.SLURMout
 
@@ -11,9 +11,9 @@
 conda="${HOME}/miniconda3"
 
 #Set variables
-threads=20
-fast5_dir="$(pwd)/fastq/ont/fast5_pass/"
-fastq="$(pwd)/fastq/ont/combined.fastq.gz"
+threads=40
+fast5_dir="fast5"
+fastq="combined.fastq"
 sequencing_summary="$(pwd)/fastq/ont/sequencing_summary_PAG35136_e70c34ec.txt"
 convert_to_single=TRUE #TRUE or FALSE, fast5 is multi-fast5 and should be converted to single-fast5
 
@@ -41,40 +41,8 @@ else
 	cd ${path2}
 fi
 
-#If data is in multi-fast5 format convert to single-fast5 format
-if [ ${convert_to_single} = TRUE ]
-then
-	if [ -d fast5 ]
-	then
-		echo "fast5 directory found in current directory"
-		echo "Assuming single-copy fast5 files are in this directory."
-		echo "Skipping conversion, if this is in error, please check, delete directory fast5, and restart."
-		fast5_dir="$(pwd)/fast5"
-	else
-		echo "Converting multi-fast5 to single-fast5"
-		multi_to_single_fast5 \
-			-t ${threads} \
-			-i ${fast5_dir} \
-			-s fast5
-		fast5_dir="$(pwd)/fast5"
-	fi
-fi
-
-#Unzip the fastq data if it is compressed
-if [[ $(pwd)/${fastq} =~ \.gz$ ]]
-then
-	if [ -f combined.fastq ]
-	then
-		echo "Uncompressed fastq file found"
-	else
-		echo "File is gzip file, uncompressing fastq"
-		gunzip -c ${fastq} > combined.fastq
-		fastq="$(pwd)/combined.fastq"
-	fi
-elif [[ ${fastq} =~ \.fastq$ ]]
-then
-	echo "File is uncompressed"
-fi
+#Remove reads that did not pass filtering from the sequencing summary
+awk '$10 != "FALSE"' ${sequencing_summary} > passed_filter_sequencing_summary.txt
 
 #Run tombo annotate_raw_with_fastqs
 echo "Running tombo preprocess annotate_raw_with_fastqs"
@@ -82,7 +50,7 @@ tombo preprocess annotate_raw_with_fastqs \
 	--processes ${threads} \
 	--fast5-basedir ${fast5_dir} \
 	--fastq-filename ${fastq} \
-	--sequencing-summary-filenames ${sequencing_summary} \
+	--sequencing-summary-filenames passed_filter_sequencing_summary.txt \
 	--basecall-group Basecall_1D_000 \
 	--basecall-subgroup BaseCalled_template \
 	--overwrite
