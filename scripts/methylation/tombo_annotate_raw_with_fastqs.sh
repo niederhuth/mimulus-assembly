e98654063696897b978e5836daf9ba6b9aa9c11e
Chad Niederhuth <niederhu@msu.com>
2022-12-01 21:46:37 -0500

    stuff

--- scripts/methylation/tombo_annotate_raw_with_fastqs.sh
+++ scripts/methylation/tombo_annotate_raw_with_fastqs.sh
@@ -41,7 +41,9 @@ else
 fi
 
 #Remove reads that did not pass filtering from the sequencing summary
-awk '$10 != "FALSE"' ${sequencing_summary} > passed_filter_sequencing_summary.txt
+awk -v OFS="\t" '{if ($1 == "filename_fastq") print $0; 
+	else if ($10 != "FALSE") print $1,$3".fast5",$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22}' \
+	${sequencing_summary} > passed_filter_sequencing_summary.txt
 
 #Run tombo annotate_raw_with_fastqs
 echo "Running tombo preprocess annotate_raw_with_fastqs"
