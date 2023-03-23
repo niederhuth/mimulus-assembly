26781526035d87c9ec05e34612fa64c3afae09f4
Niederhuth <niederhu@msu.edu>
2023-03-23 16:32:36 -0400

    stuff

--- scripts/expression/star_2pass.sh
+++ scripts/expression/star_2pass.sh
@@ -182,7 +182,7 @@ do
 	path4=$(pwd | sed s/data.*/data/)
 	for i in ${junctions_list}
 	do
-		junctions="${junctions} ${path4}/${species}/${genotype}/${i}/${datatype}_${i}_STAR_2/${sample}_SJ.out.tab"
+		junctions="${junctions} ${path4}/${species}/${genotype}/${i}/${datatype}_${i}_STAR_1/${sample}_SJ.out.tab"
 	done
 	echo "Junctions: ${junctions}"
 	STAR \
